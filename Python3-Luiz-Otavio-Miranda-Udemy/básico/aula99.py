b2acf562a0a9506d32878a79dba35d022b2c4a17
Renato <renato@IdeaPad-Slim-3.tail3910e.ts.net>
2026-04-04 17:42:05 -0300

    add aulas 93-99

---
+++ Python3-Luiz-Otavio-Miranda-Udemy/básico/aula99.py
@@ -0,0 +1,84 @@
+"""
+Calculo do primeiro dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+   10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0
+   70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 
+70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
+
+cpf = "74682489070"
+cpf_nove = cpf[:9]
+resultado_1 = 0
+
+contador_regressivo = 10
+for digito_1 in cpf_nove:
+    resultado_1 += int(digito_1) * contador_regressivo
+    contador_regressivo -= 1
+
+digito_1 = (resultado_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+"""
+Calculo do segundo dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF,
+MAIS O PRIMEIRO DIGITO,
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 11
+
+Ex.:  746.824.890-70 (7468248907)
+   11 10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
+   77 40 54 64 14 24 40 36  0 14
+
+Somar todos os resultados:
+77+40+54+64+14+24+40+36+0+14 = 363
+Multiplicar o resultado anterior por 10
+363 * 10 = 3630
+Obter o resto da divisão da conta anterior por 11
+3630 % 11 = 0
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O segundo dígito do CPF é 0
+"""
+
+cpf_dez = cpf[:10]
+resultado_2 = 0
+contador_regressivo_2 = 11
+
+for digito_2 in cpf_dez:
+    resultado_2 += int(digito_2) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+
+digito_2 = (resultado_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+# Validação do CPF
+
+cpf_check = f"{cpf_nove}{digito_1}{digito_2}"
+
+if cpf == cpf_check:
+    print(f"{cpf} é um CPF válido.")
+else:
+    print(f"{cpf} é um CPF inválido.")
